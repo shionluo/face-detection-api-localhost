3d441dcb16a2e4e5a1f35789707f67c6739eeee3
Shion <shionchill@Shions-MacBook-Pro.local>
2019-11-07 13:49:14 +0700

    Upload

---
+++ controllers/image.js
@@ -0,0 +1,33 @@
+const Clarifai = require("clarifai");
+
+const app = new Clarifai.App({
+  apiKey: "70ef9fba90494c40b74daa0e28b54129"
+});
+
+const handleApiCall = () => (req, res) => {
+  app.models
+    .predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+    .then(data => {
+      res.json(data);
+    })
+    .catch(err => res.status(400).json("Unable to work with API"));
+};
+
+const handleImage = db => (req, res) => {
+  const { id } = req.body;
+  db("users")
+    .where("id", "=", id)
+    .increment("entries", 1)
+    .returning("entries")
+    .then(entries => {
+      res.json(entries[0]);
+    })
+    .catch(err => {
+      res.status(400).json("Unable to get entries");
+    });
+};
+
+module.exports = {
+  handleImage,
+  handleApiCall
+};
---
+++ server.js
@@ -0,0 +1,44 @@
+const register = require("./controllers/register");
+const signin = require("./controllers/signin");
+const profile = require("./controllers/profile");
+const image = require("./controllers/image");
+
+const express = require("express");
+const bcrypt = require("bcrypt-nodejs");
+const cors = require("cors");
+const knex = require("knex");
+
+const db = knex({
+  client: "pg",
+  connection: {
+    host: "127.0.0.1",
+    user: "shionchill",
+    password: "",
+    database: "face-detection"
+  }
+});
+
+const app = express();
+
+app.listen(3001, () => {
+  console.log("App is running on port 3001");
+});
+
+app.use(express.json());
+app.use(cors());
+
+app.get("/", (req, res) => res.json("Tada"));
+
+//-- Signin --//
+app.post("/signin", signin.handleSignin(db, bcrypt));
+
+//-- Register --//
+app.post("/register", register.handleRegister(db, bcrypt));
+
+//-- Profile --//
+app.get("/profile/:id", profile.handleProfile(db));
+
+//-- Image --//
+app.put("/image", image.handleImage(db));
+
+app.post("/imageurl", image.handleApiCall());
